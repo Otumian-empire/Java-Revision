3aa41dbc3f89512a3d6f5d6113376fd26f7a92ec
otumian <popecan1000@gmail.com>
2019-09-25 13:02:32 +0000

    strings and chars

---
+++ src/StringsInJava.java
@@ -0,0 +1,19 @@
+public class StringsInJava {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        String sentence = "My full name is " + name;
+        System.out.println(sentence);
+
+        String firstName, lastName;
+        char firstNameInit, lastNameInit;
+
+        firstName = "Daniel";
+        lastName = "Cpu";
+        firstNameInit = 'D';
+        lastNameInit = 'C';
+
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("They have the initials, " + firstNameInit + " and " + lastNameInit);
+    }
+}
