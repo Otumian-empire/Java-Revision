bbbd7937b537f856f607055c42defa22d969ebd0
otumian <popecan1000@gmail.com>
2019-09-25 10:21:34 +0000

    comments in java

---
+++ src/CommentsInJava.java
@@ -0,0 +1,15 @@
+public class CommentsInJava {
+    public static void main(String[] args) {
+        /* multiline comments*/
+        // single line comment
+        /**
+         * Java doc
+         * This is a java documentation
+         * */
+        /*int x = 1, y = 3, sumXY = 0;
+        sumXY = x + y;
+        System.out.println(sumXY);*/
+
+
+    }
+}
