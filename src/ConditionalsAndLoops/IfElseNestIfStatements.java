44e10f7381c264298f01498ba71ee9672bb43879
otumian <popecan1000@gmail.com>
2019-09-25 13:34:48 +0000

    conditionals - if, else and nested if statements

--- src/ConditionalsAndLoops/IfElseNestIfStatements.java
+++ src/ConditionalsAndLoops/IfElseNestIfStatements.java
@@ -1,4 +1,23 @@
 package ConditionalsAndLoops;
 
 public class IfElseNestIfStatements {
+    public static void main(String[] args) {
+        /* If statements and else statements */
+        int x = 12, y = 13;
+
+        if (x < y) {
+            System.out.println(x + " is less than " + y);
+        } else {
+            System.out.println(x + " is greater than " + y);
+        }
+
+        /* nested if statements */
+        if (((x * y) + 1) < (y * y)) {
+            System.out.println("blizzard!!!");
+
+            if (x * x + y > y + x + 10) {
+                System.out.println("Blizzardoram in french");
+            }
+        }
+    }
 }
