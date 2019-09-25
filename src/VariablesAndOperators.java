48560008b7e1e8ce1afec9caf4de6f30d01a15bd
otumian <popecan1000@gmail.com>
2019-09-25 10:31:54 +0000

    variables, operators and increment and decrement

---
+++ src/VariablesAndOperators.java
@@ -0,0 +1,20 @@
+public class VariablesAndOperators {
+    public static void main(String[] args) {
+        /* Variables */
+        int age = 30;
+        String name = "John Doe";
+        System.out.println("My name is " + name + " and I am " + age + " years old");
+
+        /* operators */
+        int x = 10, y = 20, z = -2;
+        System.out.println("Add - " + (x + y));
+        System.out.println("Minus - " + (x - y));
+        System.out.println("Times - " + (x * y));
+        System.out.println("Divide - " + (x / y));
+        System.out.println("Modulo - " + (x % y));
+
+        /* increment and decrement */
+        System.out.println(x + " + 1 is the same as " + (++x));
+        System.out.println(x + " - 1 is the same as " + (--x));
+    }
+}
