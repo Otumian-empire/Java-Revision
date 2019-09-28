07be479e016623847833ea4719bb5073a40739d4
otumian <popecan1000@gmail.com>
2019-09-28 07:34:49 +0000

    Working with files

--- src/JavaFiles/WorkingWithFiles.java
+++ src/JavaFiles/WorkingWithFiles.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class WorkingWithFiles {
     public static void main(String[] args) {
-        File file = new File("hello.java");
+        File file = new File("hello");
         if (!file.exists()) {
             System.out.println(file.getName() + " does not exist");
         }
